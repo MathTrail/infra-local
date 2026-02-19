e1b43a1d77c9ba54f5d1aa19b8604170d7bd98fd
RyazanovAlexander <alexander.ryazanov.1987@gmail.com>
2026-02-18 21:47:46 -0600

    u

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 echo "MathTrail: Initializing environment..."
-chezmoi init --apply --force https://github.com/MathTrail/infra-local.git
+chezmoi init --apply --force https://github.com/MathTrail/core.git
 echo "Success: ~/.env.shared created with NAMESPACE=$(. ~/.env.shared; echo $NAMESPACE)"
