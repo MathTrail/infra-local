7ba76656d00b79532a02a2acddc3d20ea8856dad
RyazanovAlexander <alexander.ryazanov.1987@gmail.com>
2026-02-20 20:16:20 -0600

    Delete bootstrap.sh

--- scripts/bootstrap.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-echo "MathTrail: Initializing environment..."
-chezmoi init --apply --force https://github.com/MathTrail/core.git
-echo "Success: ~/.env.shared created with NAMESPACE=$(. ~/.env.shared; echo ${NAMESPACE})"
